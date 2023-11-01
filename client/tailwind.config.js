82cf11831c0038a3aed8baefebefcbde39a33a50
rajharsh81070 <rajharsh81070@gmail.com>
2023-11-01 23:07:24 +0530

    feat: client side files

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['"Inter"',  ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        "2xl": "1440px",
+      },
+    },
+  },
+  plugins: [],
+}
+
