74b70f6d80e2b816da89c9c04366385f7e26950f
rajharsh81070 <rajharsh81070@gmail.com>
2023-11-05 12:00:37 +0530

    feat: removed comments from user model

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -12,24 +12,19 @@ import bcrypt from 'bcryptjs'
 @index({ email: 1, userName: 1 }, { unique: true })
 @pre<User>('save', async function (next) {
   this.id = this._id
-  // Hash password if the password is new or was updated
   if (!this.isModified('password')) return
 
-  // Hash password with costFactor of 12
   this.password = await bcrypt.hash(this.password, 12)
   next()
 })
 @modelOptions({
   schemaOptions: {
-    // Add createdAt and updatedAt fields
     timestamps: true,
   },
   options: {
     allowMixed: Severity.ALLOW,
   },
 })
-
-// Export the User class to be used as TypeScript type
 export class User {
   @prop()
   name: string
@@ -46,12 +41,10 @@ export class User {
   @prop({ required: true, select: false })
   password: string
 
-  // Instance method to check if passwords match
   async comparePasswords(hashedPassword: string, candidatePassword: string) {
     return await bcrypt.compare(candidatePassword, hashedPassword)
   }
 }
 
-// Create the user model from the User class
 const userModel = getModelForClass(User)
 export default userModel
